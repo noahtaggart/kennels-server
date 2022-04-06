87b4db88614fe0c715b7a706f5c8f86179c6b3a9
Noah Taggart <noahtag@gmail.com>
2022-04-06 15:59:49 -0500

    made create animals and locations

--- views/__init__.py
+++ views/__init__.py
@@ -1,6 +1,6 @@
-from .animal_requests import get_all_animals, get_single_animal
+from .animal_requests import get_all_animals, get_single_animal, create_animal
 from .employee_requests import get_all_employees, get_single_employee
-from .location_requests import get_all_locations, get_single_location
+from .location_requests import get_all_locations, get_single_location, create_location
 from .customer_requests import get_all_customers, get_single_customer
 
 
